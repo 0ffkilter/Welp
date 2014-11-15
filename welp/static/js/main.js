160cea3d12c968a5b7fd6a251b664a95a040858e
Matthew Gee <geem2014@gmail.com>
2014-11-15 10:32:30 -0800

    separating functions

--- welp/static/js/main.js
+++ welp/static/js/main.js
@@ -1,13 +1,21 @@
+function sendLocation(location) { 
+    success = $.get(
+url="/",
+data = {location : [position.coords.latitude,position.coords.longitude]},
+success = function(data) 
+{ alert('page content: ' + data);
+}
+);
+
+
+
+}
+
+
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition($.get(
-                   url="/",
-                   data = {location : [position.coords.latitude,position.coords.longitude]},
-                  success = function(data) 
-                  { alert('page content: ' + data);
-                  }
-                  )
-                  );
+    navigator.geolocation.getCurrentPosition(sendLocation);
+    
   } else { 
     window.alert("Can't get your location!");  }
 }
