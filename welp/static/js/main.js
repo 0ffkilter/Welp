219da999f7c2d57f5ba5b00a9e56e82971cb15d5
Matthew Gee <geem2014@gmail.com>
2014-11-15 10:28:15 -0800

    updated json and fixed error

--- welp/static/js/main.js
+++ welp/static/js/main.js
@@ -1,10 +1,13 @@
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition($.get(
-    url="/",
-    data = {location : [position.coords.latitude,position.coords.longitude]},
-    success = function(data) 
-      { alert('page content: ' + data);}););
+                   url="/",
+                   data = {location : [position.coords.latitude,position.coords.longitude]},
+                  success = function(data) 
+                  { alert('page content: ' + data);
+                  }
+                  )
+                  );
   } else { 
     window.alert("Can't get your location!");  }
 }
